ac798267fc9ff99851845ca76a77cb1bf39273c5
DanishF1 <eksakdev@gmail.com>
2026-08-27 23:00:29 +0700

    Adding Inits

--- Init.py
+++ Init.py
@@ -1,5 +1,5 @@
 import asyncio
-
+from InitPlane import CalculatePlane
 
 class opening():
     def __init__(self, name, age):
@@ -13,11 +13,10 @@ class opening():
              2. Calculate plane params
              3. Configure drone values
              4. Calculate drone params 
-
              """))
         valuex = int(x)
 
-        if 0 < valuex < 4:
+        if 0 < valuex < 5:
             match valuex:
                 case 1:
                     ...
@@ -25,20 +24,13 @@ class opening():
                     ...
                 case 3:
                     ...
+                case 4:
+                    ...
 
         else:
             print("Invalid input")
 
-
-class childClass(opening):
-    def __init__(self, name, age, favCode):
-        super().__init__(name, age)
-        self.favCode = favCode
-
-
 name = input("Enter your name: ")
-favcode = input("Enter your favorite code: ")
-
 
 def checkage():
     age = input("Enter your age: ")
@@ -58,21 +50,11 @@ def convert():
     checkage()
     global namestr, agestr, favcodestr
     namestr = str(name)
-    favcodestr = str(favcode)
-    child = childClass(namestr, ageVal, favcodestr)
-    print(child.name, child.age, child.favCode)
-
 
 convert()
-Person = childClass(namestr, ageVal, favcodestr)
+Person = opening(namestr, ageVal, favcodestr)
 print(f"Okay, {Person.name}, you are {Person.age} right? Lets get to the interesting part")
 Person.startServices()
 
-
-async def main():
-    ...
-
-
-asyncio.run(main())
 
 
