9e95b644c52f1fa3da18e2101ac120fc675b3e05
DanishF1 <eksakdev@gmail.com>
2026-09-02 06:55:19 +0700

    Start Implementing PyQt5

--- main.py
+++ main.py
@@ -1,8 +1,71 @@
 import asyncio
+import sys
+import PyQt5
+from PyQt5 import QtWidgets
+
 import plane
 from plane import CalculatePlane
 global x
-x = 0
+from PyQt5.QtWidgets import (
+    QApplication,
+    QWidget,
+    QTextEdit,
+    QComboBox,
+    QPushButton,
+    QLabel,
+    QHBoxLayout,
+    QVBoxLayout,
+    QMessageBox,
+    QLineEdit,
+    QLabel,
+    )
+from PyQt5.QtGui import QFont
+
+class Main(QWidget):
+    def __init__(self):
+        super().__init__()
+        ...
+    def initUI(self):
+        #COLUMN 1
+        self.t1 = QLineEdit("Sweep Angle [θ]:")
+        self.t1.setReadOnly(True)
+        self.sweepAngle = QLineEdit()
+
+        self.t2 = QLineEdit("Wings Area [m²]:")
+        self.t2.setReadOnly(True)
+        self.wingsArea = QLineEdit()
+
+        self.t3 = QLineEdit("Weight [kg]:")
+        self.t3.setReadOnly(True)
+        self.weight = QLineEdit()
+
+        self.t4 = QLineEdit("Wings Length: [m]")
+        self.t4.setReadOnly(True)
+        self.wingsLength = QLineEdit()
+
+        self.t5 = QLineEdit("Tip Length [m]:")
+        self.t5.setReadOnly(True)
+        self.tipLength = QLineEdit()
+
+        self.t6 = QLineEdit("Weight [m]:")
+        self.t6.setReadOnly(True)
+        self.rootChord = QLineEdit()
+
+        self.calculate = QPushButton("Calculate")
+        self.calculate.clicked.connect(self.calculateNow)
+
+
+
+        self.master = QHBoxLayout()
+        self.col1 = QVBoxLayout()
+        self.col2 = QVBoxLayout()
+
+        self.col1.addWidget(self.t1)
+        self.col1.addWidget(self.sweepAngle)
+
+        def calculateNow(self):
+            CalculatePlane().inputData()
+
 class opening():
 
     def startServices(self):
@@ -25,7 +88,17 @@ class opening():
                 case 2:
                     ...
 
-opening().startServices()
+
+if __name__ == '__main__':
+    # Inisialisasi aplikasi
+    app = QApplication(sys.argv)
+
+    # Buat dan tampilkan jendela
+    window = SimpleApp()
+    window.show()
+
+    # Jalankan event loop
+    sys.exit(app.exec_())
 
 
 
